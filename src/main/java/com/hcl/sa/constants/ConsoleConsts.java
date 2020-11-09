670c799e9d6b65d0ea4044fcf69d534cadd2da20
lokesh.madivala <lokesh.madivala@hcl.com>
2020-11-09 10:21:42 -0800

    unused constant removed

--- src/main/java/com/hcl/sa/constants/ConsoleConsts.java
+++ src/main/java/com/hcl/sa/constants/ConsoleConsts.java
@@ -11,10 +11,10 @@ public enum ConsoleConsts {
     SITE_TYPE("siteType"), SITE_NAME("siteName"), EXTERNAL("external"), SERVER_AUTOMATION("serverAutomation"), CONSOLE_EXE_PATH(System.getenv("console_exe_path")),
     IMPORT_FIXLET("importFixlet"), BASELINE_FIXLETS_FOLDER(System.getenv("baseline_fixlets_folder_path")), BASELINE_TASKS_FOLDER(System.getenv("baseline_tasks_folder_path")),
     CUSTOM("custom"), CREATE_BASELINE("createBaselines"), BASELINE_FIXLETS_AND_TASKS_FOLDER(System.getenv("baseline_fixlets_and_tasks_folder_path")),
-    CREATE_BASELINE_PAYLOAD_PATH(System.getenv("create_baseline_payload_path")), SA("SA"), HTTP_SC_OK("200"),HTTP_SC_UNAUTH("401"),
+    CREATE_BASELINE_PAYLOAD_PATH(System.getenv("create_baseline_payload_path")), SA("SA"), HTTP_SC_OK("200"), HTTP_SC_UNAUTH("401"),
     FIXLET_LIST("fixletList"), ACTION("Action"), DELETE_PLAN("deletePlan"), CREATE_TASKS("createTasks"),
     FIXLETS_FOLDER(System.getenv("fixlets_folder_path")), PLAN_BASELINE_FIXLETS_TASKS_FOLDER(System.getenv("plan_baseline_fixlets_tasks_folder")), TASKS_FOLDER(System.getenv("tasks_folder_path")), FIXLETS_TASKS_FOLDER(System.getenv("fixlets_and_tasks_folder_path")),
-    TAKE_ACTION_PAYLOAD_PATH(System.getenv("take_action_payload_path")), MASTER_ACTION_SITE("Master Action Site"), SRC_SITENAME("CustomSite_Pooja"),
+    TAKE_ACTION_PAYLOAD_PATH(System.getenv("take_action_payload_path")), MASTER_ACTION_SITE("Master Action Site"),
     FIXLET("Fixlet"), ROOT_SERVER(System.getenv("bigfix_server_name")), TASK("Task"), PLAN_ACTION("executePlanAction"), SECURE_PARAMETER_FIXLET_FOLDER_PATH(System.getenv("secure_parameter_fixlet_folder_path"));
 
     public String text;
